689d026a2d6a38a0269fddf2cfad52e79a0b12e6
Tomas Martin <martintomas898@gmail.com>
2025-05-13 19:47:33 -0300

    add route y cronjob

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import router from "./routes/projectRoutes"
 import authRouter from "./routes/authRouter"
 import cors from "cors";
 import morgan from "morgan";
-
+import cron from "node-cron";
 
 dotenv.config()
 
@@ -27,5 +27,14 @@ app.use(express.json())
 // Routes
 app.use("/api/auth", authRouter)
 app.use("/api/projects", router)
+
+// Route for CronJob
+app.get('/ping', (req, res) => {
+  res.send('Servidor activo ✅');
+});
+
+cron.schedule('*/59 * * * *', () => {
+  console.log('⏰ Ejecutando tarea interna...');
+});
 
 export default app
